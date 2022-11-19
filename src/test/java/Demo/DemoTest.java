28610b90e8a55b4f4f647fb88c1576b8b4fd4b33
Dell <Dell@Praveenkumar>
2022-11-19 11:56:03 +0530

    Commit 1-DemoTest

--- src/test/java/Demo/DemoTest.java
+++ src/test/java/Demo/DemoTest.java
@@ -7,7 +7,8 @@ public class DemoTest
    @Test
    public void demo()
    {
-	   System.out.println("HI This is WASM6 Framework");
+	   System.out.println("HI This is WASM6 Framework by Chaitra Mam");
 	   System.out.println("Welcome");
+	   System.out.println("Github");
    }
 }
