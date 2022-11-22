21ab9927983c27f04dbcf7d687154a8e722ea347
Ajay <Dell@Praveenkumar>
2022-11-22 11:20:15 +0530

    Commit -Pull request for New branch

---
+++ src/test/java/Demo/PracticeTestForPullRequestGitHub.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class PracticeTestForPullRequestGitHub
+{
+  @Test
+  public void pullRequest()
+  {
+	  System.out.println("Create Pull Request");
+  }
+}
